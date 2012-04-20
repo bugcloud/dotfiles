16206db2b72eb983f975c909264c9292769cc545
bugcloud <bugcloudisco@bugcloud.com>
2012-04-20 13:23:57 +0900

    add itermcolors

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DOT_FILES=( .zshrc .gitconfig .gitignore .gitscripts .irbrc .vimrc .vim .tmux.conf .conkycolors bin)
+DOT_FILES=( .zshrc .gitconfig .gitignore .gitscripts .irbrc .vimrc .vim .tmux.conf .conkycolors bin itermcolors)
 for file in ${DOT_FILES[@]}
 do
   ln -s $HOME/dotfiles/$file $HOME/$file
