c72b7f8460ad3a4088d82ef5c08f9284d6e47e5d
bugcloud <bugcloudisco@bugcloud.com>
2013-06-12 13:31:01 +0900

    Add uncrustify.cfg

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DOT_FILES=( .zshrc .gitconfig .gitignore .gitscripts .irbrc .vimrc .vim .tmux.conf .autotest .conkycolors bin itermcolors .pryrc )
+DOT_FILES=( .zshrc .gitconfig .gitignore .gitscripts .irbrc .vimrc .vim .tmux.conf .autotest .conkycolors bin itermcolors .pryrc uncrustify.cfg )
 for file in ${DOT_FILES[@]}
 do
   ln -s $HOME/dotfiles/$file $HOME/$file
