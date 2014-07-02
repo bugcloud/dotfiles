44d43d3e9de0eec5929bd528602ad23192b02569
bugcloud <bugcloudisco@bugcloud.com>
2014-07-02 11:45:56 +0900

    update setup script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DOT_FILES=( .zshrc .gitconfig .gitignore .gitscripts .irbrc .vimrc .vim .tmux.conf .autotest .conkycolors bin itermcolors .pryrc uncrustify.cfg Brewfile )
+DOT_FILES=( .zshrc .gitconfig .gitignore .gitscripts .irbrc .vimrc .vim .tmux.conf .autotest .editorconfig bin itermcolors .pryrc uncrustify.cfg Brewfile )
 for file in ${DOT_FILES[@]}
 do
   ln -s $HOME/dotfiles/$file $HOME/$file
