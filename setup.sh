3ca96910416f7bbd14daefa05f3f29acfc55a3df
bugcloud <bugcloudisco@bugcloud.com>
2016-05-05 21:48:28 +0900

    oh-my-zsh -> .prezto

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DOT_FILES=( .zshrc .gitconfig .gitignore .gitscripts .irbrc .vimrc .vim .tmux.conf .editorconfig bin itermcolors .pryrc uncrustify.cfg Brewfile )
+DOT_FILES=( .zshrc .zpreztorc .gitconfig .gitignore .gitscripts .vimrc .vim .tmux.conf .editorconfig bin itermcolors .pryrc uncrustify.cfg )
 for file in ${DOT_FILES[@]}
 do
   ln -s $HOME/dotfiles/$file $HOME/$file
@@ -10,3 +10,9 @@ wget -O ~/.gibo-completion.zsh https://raw.githubusercontent.com/simonwhitaker/g
 
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
+
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
