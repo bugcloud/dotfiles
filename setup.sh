fe8542d5944744fe17456cb21ff689e274adb39e
bugcloud <bugcloudisco@bugcloud.com>
2016-08-24 16:05:41 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-DOT_FILES=( .zshrc .zpreztorc .gitconfig .gitignore .gitscripts .gitattributes .vimrc .vim .tmux.conf .editorconfig bin itermcolors .pryrc uncrustify.cfg )
-for file in ${DOT_FILES[@]}
-do
-  ln -s $HOME/dotfiles/$file $HOME/$file
-done
-
 wget -O ~/.gibo-completion.zsh https://raw.githubusercontent.com/simonwhitaker/gibo/master/gibo-completion.zsh
 
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
@@ -15,6 +9,12 @@ git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$H
 setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
   ln -snfv "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
+
+DOT_FILES=( .zshrc .zpreztorc .zshenv .gitconfig .gitignore .gitscripts .gitattributes .vimrc .vim .tmux.conf .editorconfig bin itermcolors .pryrc uncrustify.cfg )
+for file in ${DOT_FILES[@]}
+do
+  ln -snfv $HOME/dotfiles/$file $HOME/$file
 done
 
 mkdir ${HOME}/.config
